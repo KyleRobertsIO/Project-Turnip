aedd483b24d1a1b59143fd4f7c00060be6a4b482
Kyle Roberts <kkroberts1635@gmail.com>
2019-05-02 15:14:26 -0400

    match css

--- js/loadMatchStats.js
+++ js/loadMatchStats.js
@@ -1,5 +1,27 @@
+let SC = require('../SlippiCollection.js');
+
 let index = sessionStorage.getItem('match-breakdown-index');
 let fileCollection = JSON.parse(sessionStorage.getItem('fileCollection'));
 let filePath = fileCollection[index];
 
 console.log(fileCollection[index]);
+
+let settings = SC.readSlippiSettings(filePath);
+let characters = SC.getMatchCharcters(settings);
+
+let headerLeft = document.getElementById('stage-image-container');
+setCharacterIcons(characters, headerLeft);
+
+console.log(characters);
+
+function setCharacterIcons(characters, container){
+    for(let i = 0; i < characters.length; i++){
+        let icon = document.createElement('img');
+        icon.setAttribute('src', `../stocks/${characters[i].name}/${characters[i].icon}`);
+        icon.classList.add('character-icon');
+        let iconContainer = document.createElement('div');
+        iconContainer.classList.add('character-icon-container');
+        iconContainer.appendChild(icon);
+        container.appendChild(iconContainer);
+    }
+}
